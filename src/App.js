6799dae5672e5e278cf6c0c90d50d91dce379f68
Shahroz Ahmed <shahrozahmed286@gmail.com>
2020-07-29 03:19:37 +0500

    fix some mistakes in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class App extends Component() {
-render(){
-
-  <div>
-    <h1>Hello World</h1>
-  </div>
-}
+class App extends Component{
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
