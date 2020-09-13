dc09998c2bb829d4653572533239ba3ce6e417b3
Shahroz Ahmed <shahrozahmed286@gmail.com>
2020-09-13 12:33:10 +0500

    Updates

---
+++ precache-manifest.9703cc8d2f943f71173b1a4a01bb5c6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa74bad7cfaa014c818a0eaa4e94b41c",
+    "url": "/burger_builder/index.html"
+  },
+  {
+    "revision": "5b66824b7b33234a9b21",
+    "url": "/burger_builder/static/css/main.24b6a65a.chunk.css"
+  },
+  {
+    "revision": "4a981c94766fb2036d42",
+    "url": "/burger_builder/static/js/2.f3a809d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger_builder/static/js/2.f3a809d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b66824b7b33234a9b21",
+    "url": "/burger_builder/static/js/main.870627cb.chunk.js"
+  },
+  {
+    "revision": "a22ee43ba3f217dbcc32",
+    "url": "/burger_builder/static/js/runtime-main.61cf9bbb.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger_builder/static/media/logo.b8503d26.png"
+  }
+]);
