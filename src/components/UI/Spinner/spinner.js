a31a14350b77b727797d7678ebf3e1605514de7e
Shahroz Ahmed <shahrozahmed286@gmail.com>
2020-10-19 16:24:58 +0500

    added a loader functionality when submitting the request on firebase.

---
+++ src/components/UI/Spinner/spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './spinner.module.css'
+
+const Spinner = () => {
+    return (
+        <div className = {classes.Loader}>Loading...</div>
+    )
+}
+
+export default Spinner;
--- src/containers/BurgerBuilder/burger_builder.js
+++ src/containers/BurgerBuilder/burger_builder.js
@@ -4,7 +4,8 @@ import Burger from "../../components/Burger/burger";
 import BuildControls from "../../components/Burger/Build Controls/build_controls";
 import Modal from "../../components/UI/Modal/modal";
 import OrderSummary from "../../components/Burger/Order Summary/order_summary";
-import axios from '../../axios-order'
+import axios from "../../axios-order";
+import Spinner from "../../components/UI/Spinner/spinner";
 
 const INGREDIENTS_PRICES = {
   cheese: 2,
@@ -24,6 +25,7 @@ export default class BurgerBuilder extends Component {
     totalPrice: 5,
     isPurchasable: false,
     OrderClicked: false,
+    loading: false,
   };
 
   //validate if order button should be active or disable
@@ -92,25 +94,26 @@ export default class BurgerBuilder extends Component {
   };
 
   continueOrder = () => {
- 
+    this.setState({loading:true});
     const order = {
       ingredient: this.state.ingredients,
       price: this.state.totalPrice,
       customerData: {
-        name: 'shahroz',
-        address: 'shadman town, karachi',
-        email: 'shahrozahmed286@gmail.com'
-      }
-    }
+        name: "shahroz",
+        address: "shadman town, karachi",
+        email: "shahrozahmed286@gmail.com",
+      },
+    };
 
-    axios.post('/orders.json' , order).then(response => {
-      console.log(response)
-    }).catch(error =>{
-      console.log(error)
-    })
-
+    axios
+      .post("/orders.json", order)
+      .then((response) => {
+        this.setState({loading:false,OrderClicked:false });
+      })
+      .catch((error) => {
+        this.setState({loading:false,OrderClicked:false });
+      });
   };
-
 
   render() {
     const disableInfo = {
@@ -119,14 +122,23 @@ export default class BurgerBuilder extends Component {
     for (const key in disableInfo) {
       disableInfo[key] = disableInfo[key] <= 0;
     }
+
+    let orderSummary = (
+      <OrderSummary
+        ingredients={this.state.ingredients}
+        cancleorder={this.removeModal}
+        continueorder={this.continueOrder}
+      />
+    );
+
+    if (this.state.loading) {
+      orderSummary = <Spinner />;
+    }
+
     return (
       <Aux>
         <Modal show={this.state.OrderClicked} removebackdrop={this.removeModal}>
-          <OrderSummary
-            ingredients={this.state.ingredients}
-            cancleorder={this.removeModal}
-            continueorder={this.continueOrder}
-          />
+          {orderSummary}
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
