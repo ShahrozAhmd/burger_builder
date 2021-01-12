1348fff5dd381b3ea01963240894e237274078f7
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-01-12 19:03:56 +0500

    setup reducer and actions for some states

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./store/reducer";
+
+const store  = createStore(reducer);
+
 const app = (
-  <Router>
-    <App />
-  </Router>
+  <Provider>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
---
+++ src/store/reducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+  ingredients: null,
+  totalPrice: 5,
+};
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
