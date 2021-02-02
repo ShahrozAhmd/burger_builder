72091cd6fe29fc00f0808c235643a8ec9c660d57
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-02-02 15:20:12 +0500

    middleware added

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import burgerBuilderReducers from "./store/reducers/burger_builder_reducers";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   burgerBuilderReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
+  
 );
 
 const app = (
