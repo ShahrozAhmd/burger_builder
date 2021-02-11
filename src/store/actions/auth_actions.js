e1840e49377b54c273e0244cef82964cfee89def
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-02-11 18:55:57 +0500

    signup functionality with authentication has been tested using firebase

--- src/store/actions/auth_actions.js
+++ src/store/actions/auth_actions.js
@@ -1,27 +1,46 @@
-import * as actionTypes from './actionTypes';
+import axios from "axios";
+import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
-    return{
-        type: actionTypes.AUTH_START
-    }
-}
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
 
 export const authSuccess = (authData) => {
-    return{
-        type: actionTypes.AUTH_SUCCESS,
-        authData : authData,
-    }
-}
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData,
+  };
+};
 
 export const authFail = (err) => {
-    return{
-        type: actionTypes.AUTH_FAIL,
-        error: err,
-    }
-}
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: err,
+  };
+};
 
 export const authenticate = (email, pass) => {
-    return dispatch => {
-        dispatch(authStart())
-    }
-}
+  return (dispatch) => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: pass,
+      returnSecureToken: true,
+    };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBy4c6DaAfhkiDB6TP06-rFyhScM1LuZek",
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
+  };
+};
