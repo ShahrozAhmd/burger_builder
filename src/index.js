ecc6e8272d4afba3d1600e66c6684db876984eb9
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-02-21 21:44:10 +0500

    secure redux dev tools for production mode using environment variables

--- src/index.js
+++ src/index.js
@@ -6,23 +6,24 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import burgerBuilderReducers from "./store/reducers/burger_builder_reducers";
-import ordersReducers from './store/reducers/order_reducers'
-import authReducers  from './store/reducers/auth_reducers'
+import ordersReducers from "./store/reducers/order_reducers";
+import authReducers from "./store/reducers/auth_reducers";
 import thunk from "redux-thunk";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducers,
   orders: ordersReducers,
   authentication: authReducers,
-})
+});
 
 const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk))
-  
 );
 
 const app = (
